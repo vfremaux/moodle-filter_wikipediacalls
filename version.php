fda922caa58632eb7f5fe18e20933d72bba928ce
valery <valery.fremaux@gmail.com>
2018-03-01 16:56:52 +0100

    resync all and push missig branches

--- version.php
+++ version.php
@@ -27,10 +27,10 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2013010100;        // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires  = 2017110800;        // Requires this Moodle version.
+$plugin->requires  = 2017050500;        // Requires this Moodle version.
 $plugin->component = 'filter_wikipediacalls'; // Full name of the plugin (used for diagnostics).
-$plugin->release = "3.4.0 (build 2013010100)";
+$plugin->release = "3.3.0 (build 2013010100)";
 $plugin->maturity = MATURITY_STABLE;
 
 // Non moodle attributes.
-$plugin->codeincrement = '3.4.0000';
+$plugin->codeincrement = '3.3.0000';
